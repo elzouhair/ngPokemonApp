1a9cd33b5c1bfcff8681a6f8432aea53a3693ad4
elkilizouhair <elkilizouhair@gmail.com>
2020-12-11 16:37:13 +0100

    fixed distinctUntilChanged() error by adding <string> in searchTerms declaration

--- src/app/pokemons/search-pokemon/search-pokemon.component.ts
+++ src/app/pokemons/search-pokemon/search-pokemon.component.ts
@@ -11,15 +11,20 @@ import { PokemonsService } from '../pokemons.service';
   styleUrls: ['./search-pokemon.component.scss']
 })
 export class SearchPokemonComponent implements OnInit {
-  private searchTerms = new Subject();
+  private searchTerms = new Subject<string>();
   pokemons$: Observable<Pokemon[]>=null as any;
   constructor(private router:Router,private pokemonsService:PokemonsService) { }
 
+  
   ngOnInit(): void {
     this.pokemons$ = this.searchTerms.pipe(
+      // attendre 300ms de pause entre chaque requête
       debounceTime(300),
+      // ignorer la recherche en cours si c'est la même que la précédente
       distinctUntilChanged(),
-      switchMap((term: string) => this.pokemonsService.searchPokemons(term))
+      // on retourne la liste des résultats correpsondant aux termes de la recherche
+      // ça annule et rejette les termes de recherches et retourne seulement le plus récent.
+      switchMap((term: string) => this.pokemonsService.searchPokemons(term)),
     );
   }
   search(term:string):void {
